dbdc45e2513575af13555b806757f4cb0b0a2a2d
blcksgnota <gilbert@sgnota.com>
2022-06-16 18:32:09 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print a-z && A-Z
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int  i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
+}
